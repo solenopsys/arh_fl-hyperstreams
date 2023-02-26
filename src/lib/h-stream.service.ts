46ddb38768a6191c445487ea9360079758f5b2f4
AlexStorm <info@alexstorm.ru>
2023-02-26 15:34:36 +0530

    name refactoring

--- src/lib/h-stream.service.ts
+++ src/lib/h-stream.service.ts
@@ -5,73 +5,73 @@ import {AddSocket, UpdateServiceMapping} from "./hstreams.store";
 import {StreamsPool, WsPool} from "./ws-pool";
 import {Observable} from "rxjs";
 import {DataHstreamModule} from "./data-hstream.module";
-import {Cluster, ClusterState} from "@solenopsys/fl-clusters";
+import {ClusterState, Cluster} from "@solenopsys/fl-clusters";
 
 
 @Injectable({
-  providedIn: DataHstreamModule
+    providedIn: DataHstreamModule
 })
 export class HStreamService {
-  @Select(ClusterState.getClusters) clusters$!: Observable<Cluster[]>;
+    @Select(ClusterState.getClusters) clusters$!: Observable<Cluster[]>;
 
 
-  constructor(private store: Store, private sp: StreamsPool, private ws: WsPool) {
+    constructor(private store: Store, private sp: StreamsPool, private ws: WsPool) {
 
-    setTimeout(() => {
-      const currentCluster = this.store.selectSnapshot(ClusterState.getClusters);
-      for (const cluster of currentCluster) {
-        let wsUrl = this.genWsUrl(cluster);
-        let infoUrl = this.genInfoUrl(cluster);
-        store.dispatch(new AddSocket(wsUrl));
-        store.dispatch(new UpdateServiceMapping(wsUrl,infoUrl));
-      }
+        setTimeout(() => {
+            const currentCluster: any = this.store.selectSnapshot(ClusterState.getClusters);
+            for (const cluster of currentCluster) {
+                let wsUrl: string = this.genWsUrl(cluster);
+                let infoUrl: string = this.genInfoUrl(cluster);
+                store.dispatch(new AddSocket(wsUrl));
+                store.dispatch(new UpdateServiceMapping(wsUrl, infoUrl));
+            }
 
-      //    this.clusters$.subscribe(clusters$)
-    });
-
-
-  }
+            //    this.clusters$.subscribe(clusters$)
+        });
 
 
-  public genWsUrl(cluster: Cluster): string {
-    const protocol = cluster.ssl ? "wss" : "ws";
-    return `${protocol}://${cluster.host}/hs/websocket`;
-  }
-
-  public genInfoUrl(cluster: Cluster): string {
-    const protocol = cluster.ssl ? "https" : "http";
-    return `${protocol}://${cluster.host}/hs/services`;
-  }
+    }
 
 
-  createStringQuery(service: string, query: string, funcId: number): Observable<any> {
-    const currentCluster = this.store.selectSnapshot(ClusterState.getCurrent);
-    return this.createStringQueryCluster(this.genWsUrl(currentCluster), service, query, funcId);
-  }
+    public genWsUrl(cluster: Cluster): string {
+        const protocol = cluster.ssl ? "wss" : "ws";
+        return `${protocol}://${cluster.host}/hs/websocket`;
+    }
+
+    public genInfoUrl(cluster: Cluster): string {
+        const protocol = cluster.ssl ? "https" : "http";
+        return `${protocol}://${cluster.host}/hs/services`;
+    }
 
 
-  createStringQueryCluster(url: string, service: string, query: string, funcId: number): Observable<any> {
-    return this.createBinaryQueryCluster(url, service, new TextEncoder().encode(query), funcId);
-  }
+    createStringQuery(service: string, query: string, funcId: number): Observable<any> {
+        const currentCluster = this.store.selectSnapshot(ClusterState.getCurrent);
+        return this.createStringQueryCluster(this.genWsUrl(currentCluster), service, query, funcId);
+    }
 
-  createBinaryQuery(service: string, query: ArrayBuffer, funcId: number): Observable<any> {
-    const currentCluster = this.store.selectSnapshot(ClusterState.getCurrent);
-    return this.createBinaryQueryCluster(this.genWsUrl(currentCluster), service, query, funcId);
-  }
 
-  createBinaryQueryCluster(url: string, service: string, query: ArrayBuffer, funcId: number): Observable<ArrayBuffer> {
-    const hStream = this.sp.createStream(service, url);
-    let socket = this.ws.sockets[url];
+    createStringQueryCluster(url: string, service: string, query: string, funcId: number): Observable<any> {
+        return this.createBinaryQueryCluster(url, service, new TextEncoder().encode(query), funcId);
+    }
 
-    hStream.output.subscribe((z:any)=> {
-      socket.output.next(z);
-    });
+    createBinaryQuery(service: string, query: ArrayBuffer, funcId: number): Observable<any> {
+        const currentCluster = this.store.selectSnapshot(ClusterState.getCurrent);
+        return this.createBinaryQueryCluster(this.genWsUrl(currentCluster), service, query, funcId);
+    }
 
-    setTimeout(() => {
-      hStream.send(query, funcId,true);
-    });
-    return hStream.pipe();
-  }
+    createBinaryQueryCluster(url: string, service: string, query: ArrayBuffer, funcId: number): Observable<ArrayBuffer> {
+        const hStream = this.sp.createStream(service, url);
+        let socket = this.ws.sockets[url];
+
+        hStream.output.subscribe((z: any) => {
+            socket.output.next(z);
+        });
+
+        setTimeout(() => {
+            hStream.send(query, funcId, true);
+        });
+        return hStream.pipe();
+    }
 
 
 }
